7331aa4b9116f84a19e44183e66ce3d5855a78d9
Nick Ochiel <nochiel@users.noreply.github.com>
2024-03-30 16:17:15 +0300

    Add server API crate

--- bc-server-api/src/api.rs
+++ bc-server-api/src/api.rs
@@ -1,3 +1,9 @@
+use warp::{
+    http::StatusCode,
+    reject::Rejection,
+    reply::{self, Reply},
+};
+
 pub const MAX_DATA_SIZE: u32 = 1000;
 pub const CONTINUATION_EXPIRY_SECONDS: u32 = 60 * 60 * 24;
 
@@ -16,9 +22,3 @@ impl warp::reject::Reject for InvalidBody {}
 #[derive(Debug)]
 pub struct AnyhowError(anyhow::Error);
 impl warp::reject::Reject for AnyhowError {}
-
-use warp::{
-    http::StatusCode,
-    reject::Rejection,
-    reply::{self, Reply},
-};
